301e4224ff2c77e1dbd62362b98ac8a139c4c712
jballmann <jochen.ballmann@am-gmbh.de>
2024-07-25 13:29:28 +0200

    setup pabu

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+module.exports = () => ({
+  pabu: {
+    enabled: true,
+    resolve: "./src/plugins/pabu",
+  },
+  upload: {
+    config: {
+      sizeLimit: 250 * 1024 * 1024,
+    },
+  },
+});
